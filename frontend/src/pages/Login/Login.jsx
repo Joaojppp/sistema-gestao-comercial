ae9af7823ff24f7bb34b9d9f963f00fd7df2182b
Joaojppp <joaopp2015br@gmail.com>
2026-07-31 01:38:33 -0300

    Adiciona tela de login e configuração da Vercel

--- frontend/src/pages/Login/Login.jsx
+++ frontend/src/pages/Login/Login.jsx
@@ -1,9 +1,56 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+import "../../styles/login.css";
+
 function Login() {
+
+    const [email, setEmail] = useState("");
+    const [senha, setSenha] = useState("");
+
+    const navigate = useNavigate();
+
+    function entrar(){
+
+        // por enquanto apenas entra no sistema
+        navigate("/dashboard");
+
+    }
+
     return(
-        <hi>Login</hi>
 
+        <div className="login-container">
 
-    );  
+            <div className="login-box">
+
+                <h1>Sistema Comercial</h1>
+
+                <input
+                    type="email"
+                    placeholder="Email"
+                    value={email}
+                    onChange={(e)=>setEmail(e.target.value)}
+                />
+
+                <input
+                    type="password"
+                    placeholder="Senha"
+                    value={senha}
+                    onChange={(e)=>setSenha(e.target.value)}
+                />
+
+                <button onClick={entrar}>
+
+                    Entrar
+
+                </button>
+
+            </div>
+
+        </div>
+
+    );
+
 }
 
 export default Login;
